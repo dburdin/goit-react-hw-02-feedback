9c67d92204cb8b391a4939477719082ea988e211
Dmytrii Bevziuk <113050865+dburdin@users.noreply.github.com>
2023-03-31 14:56:15 +0300

    hm-react-feedback

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,59 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Wrapper } from './App.styled';
+import { FeedBackOptions } from './FeedBackOptions/FeedBackOptions';
+import { Statistics } from './Statistics/Statistics';
+import { Section } from './Section/Section';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onLeaveFeedback = event => {
+    this.setState(prev => ({ [event]: prev[event] + 1 }));
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+  countPositiveFeedbackPercentage = () => {
+    const total = this.countTotalFeedback();
+    return Math.round((this.state.good * 100) / total);
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    return (
+      <Wrapper>
+        <Section
+          title="Please leave feedback"
+          children={
+            <FeedBackOptions
+              options={Object.keys(this.state)}
+              onLeaveFeedback={this.onLeaveFeedback}
+            />
+          }
+        ></Section>
+        <Section
+          title="Statistics"
+          children={
+            this.countTotalFeedback() > 0 ? (
+              <Statistics
+                good={good}
+                neutral={neutral}
+                bad={bad}
+                total={this.countTotalFeedback()}
+                positivePercentage={this.countPositiveFeedbackPercentage()}
+              ></Statistics>
+            ) : (
+              'No feedback given'
+            )
+          }
+        ></Section>
+      </Wrapper>
+    );
+  }
+}
---
+++ src/components/Section/Section.styled.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Div = styled.div`
+  width: 400px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  :not(:last-child) {
+    margin-bottom: 30px;
+  }
+  color: white;
+  align-items: center;
+`;
+
+export const Title = styled.h2`
+  color: white;
+  text-align: center;
+`;
